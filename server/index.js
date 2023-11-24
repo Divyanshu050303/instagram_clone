714b51af62443139a3a3cf9cbef7c078581201df
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-24 23:31:10 +0530

    connect to the mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 // Imports 
 import express from 'express';
 import { connect } from 'mongoose';
+import  authRouter from "./routes/auth.js";
 
 const PORT=process.env.PORT || 3000
 const app= express();
 const DB="mongodb+srv://divyanshu:12345@cluster0.wlivs3w.mongodb.net/?retryWrites=true&w=majority"
-
-
+app.use(express.json());
+app.use(authRouter);
 // connection
 
 connect(DB).then(()=>{
@@ -15,6 +16,6 @@ connect(DB).then(()=>{
     console.log(e);
 })
  
-app.listen(PORT, "0.0.0.0",()=>{
+app.listen(PORT,()=>{
     console.log(`connected at port ${PORT}`);
 });
