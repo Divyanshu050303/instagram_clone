a7a2948600b706d7784cde8da74d616d8d4a8fd4
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-25 21:46:51 +0530

    user provider

---
+++ lib/Provider/user_provider.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user.dart';
+
+class UserProvider extends ChangeNotifier{
+  User _user=User(
+    id: '',
+    name: '',
+    email: '',
+    password: '',
+    token: ''
+  );
+  User get user=> _user;
+  void setUser(String user){
+    _user=User.fromJson(user);
+    notifyListeners();
+  }
+  void setUserFromModel(User user){
+    _user=user;
+    notifyListeners();
+  }
+}
