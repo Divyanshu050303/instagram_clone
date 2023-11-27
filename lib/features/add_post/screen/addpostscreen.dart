c1f672df6ec07af6d4612eaa1b3db9bb6fc3b86b
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-27 22:14:08 +0530

    add post screen

---
+++ lib/features/add_post/screen/addpostscreen.dart
@@ -0,0 +1,137 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/constant/utils.dart';
+
+import '../../../common/widget/custom_textfield.dart';
+
+
+class AddPost extends StatefulWidget {
+  static const String routeName = '/add-post';
+  const AddPost({super.key});
+
+  @override
+  State<AddPost> createState() => _AddPostState();
+}
+
+class _AddPostState extends State<AddPost> {
+  final TextEditingController productNameController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+  final TextEditingController priceController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+  String category = 'Mobiles';
+  List<File> images = [];
+  final _addProductFormKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    super.dispose();
+    productNameController.dispose();
+    descriptionController.dispose();
+    priceController.dispose();
+    quantityController.dispose();
+  }
+
+  void selectImages() async {
+    var res = await pickImages();
+    setState(() {
+      images = res;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData=MediaQuery.of(context);
+    return  Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          title: const Text(
+            'Add Product',
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _addProductFormKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              children: [
+                  SizedBox(height:mediaQueryData.size.height*0.2),
+                images.isNotEmpty
+                    ? CarouselSlider(
+                  items: images.map(
+                        (i) {
+                      return Builder(
+                        builder: (BuildContext context) => Image.file(
+                          i,
+                          fit: BoxFit.cover,
+                          height: 200,
+                        ),
+                      );
+                    },
+                  ).toList(),
+                  options: CarouselOptions(
+                    viewportFraction: 1,
+                    height: 200,
+                  ),
+                )
+                    : GestureDetector(
+                  onTap: selectImages,
+                  child: DottedBorder(
+                    borderType: BorderType.RRect,
+                    radius: const Radius.circular(10),
+                    dashPattern: const [10, 4],
+                    strokeCap: StrokeCap.round,
+                    child: Container(
+                      width: double.infinity,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Icon(
+                            Icons.folder_open,
+                            size: 40,
+                          ),
+                          const SizedBox(height: 15),
+                          Text(
+                            'Select Images',
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.grey.shade400,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                CustomTextField(
+                  controller: productNameController,
+                  hintText: 'Name',
+                ),
+                const SizedBox(height: 10),
+                CustomTextField(
+                  controller: descriptionController,
+                  hintText: 'Description',
+                ),
+                const SizedBox(height: 10),
+                 ElevatedButton(onPressed: (){}, child: Text("Add Post"))
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
