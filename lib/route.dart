680f7933835c610d6c4e74e364c1d9526ac94849
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-27 22:16:39 +0530

    add to more router for add post screen and reel screen

--- lib/route.dart
+++ lib/route.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/common/widget/bottom_bar.dart';
 import 'package:instagram_clone/features/Home/screen/home_screen.dart';
+import 'package:instagram_clone/features/add_post/screen/addpostscreen.dart';
 import 'package:instagram_clone/features/authentication/screen/auth_screen.dart';
 import 'package:instagram_clone/features/profile/screen/Profile.dart';
+import 'package:instagram_clone/features/reel/screen/reel.dart';
 import 'package:instagram_clone/features/search/Screen/Search.dart';
 
 Route<dynamic> generateRoute(RouteSettings routeSettings) {
   switch (routeSettings.name) {
     case AuthScreen.routeName:
-      return MaterialPageRoute(
-          settings:routeSettings,
-        builder: (_)=>const AuthScreen()
-      );
-      case HomeScreen.routeName:
+       return MaterialPageRoute(settings:routeSettings, builder: (_)=>const AuthScreen());
+       break;
+    case HomeScreen.routeName:
       return MaterialPageRoute(
           settings:routeSettings,
         builder: (_)=>HomeScreen()
@@ -31,6 +31,16 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
       return MaterialPageRoute(
           settings:routeSettings,
         builder: (_)=> const Profile()
+      );
+      case AddPost.routeName:
+      return MaterialPageRoute(
+          settings:routeSettings,
+        builder: (_)=> const AddPost()
+      );
+      case Reel.routeName:
+      return MaterialPageRoute(
+          settings:routeSettings,
+        builder: (_)=> const Reel()
       );
     default:
       return MaterialPageRoute(settings: routeSettings, builder: (_)=>const Scaffold(
