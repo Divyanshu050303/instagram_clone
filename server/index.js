28c251334b7d1b62dd60655979972f60eed3c2bb
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-28 19:10:05 +0530

    add post router

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@
 import express from 'express';
 import { connect } from 'mongoose';
 import  authRouter from "./routes/auth.js";
+import  postRouter from "./routes/post.js";
 
 const PORT=process.env.PORT || 3000
 const app= express();
 const DB="mongodb+srv://divyanshu:12345@cluster0.wlivs3w.mongodb.net/?retryWrites=true&w=majority"
 app.use(express.json());
 app.use(authRouter);
+app.use(postRouter);
 // connection
 
 connect(DB).then(()=>{
