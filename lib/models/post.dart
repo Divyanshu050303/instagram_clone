b3e9f523f519897c026a20116aa397e5b7ba9183
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-11-29 22:20:08 +0530

    change the image type

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,27 +1,27 @@
 import 'dart:convert';
 
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 class Post {
   final String name;
   final String description;
   final double like;
-  final String? id;
   final List<String> images;
+  final String? id;
   Post({
     required this.name,
     required this.description,
     required this.like,
-    this.id,
     required this.images,
+    this.id,
   });
 
   Map<String, dynamic> toMap() {
-    return <String, dynamic>{
+    return {
       'name': name,
       'description': description,
-      'like': like,
-      'id':id,
+      'quantity': like,
       'images': images,
+      'id': id,
     };
   }
 
@@ -31,12 +31,12 @@ class Post {
       description: map['description'] ?? '',
       like: map['like']?.toDouble() ?? 0.0,
       images: List<String>.from(map['images']),
-      id:map['_id'],
+      id: map['_id'],
     );
   }
 
   String toJson() => json.encode(toMap());
 
   factory Post.fromJson(String source) =>
-      Post.fromMap(json.decode(source) as Map<String, dynamic>);
+      Post.fromMap(json.decode(source));
 }
